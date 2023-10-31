cf80df1e12db440283dafd733ce88f4bc3c09d60
dreyesliranzo <145797349+dreyesliranzo@users.noreply.github.com>
2023-10-31 13:59:05 -0400

    added unity proyect

---
+++ bounce/Assets/ballSpeed.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ballSpeed : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    public float jump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+
+       
+        //player moves left
+        if (Input.GetKey("a"))
+        {
+            newPosition.x -= speed;
+        }
+
+        //player moves right
+        if (Input.GetKey("d"))
+        {
+            newPosition.x += speed;
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+        }
+
+
+        transform.position = newPosition;
+    }
+
+    private void Launch()
+
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("spikes"))
+        {
+
+            Destroy(gameObject);
+            SceneManager.LoadScene(1);
+
+
+        }
+    }
+
+
+}
